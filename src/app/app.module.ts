dab810474cc29c16fcd8706fc7fd1b96ea192d63
Fernando Santillan <ferdiego88@gmail.com>
2021-03-07 23:21:45 -0500

    fsv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { FotosComponent } from './components/fotos/fotos.component';
 import { CargaComponent } from './components/carga/carga.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
+// Angular firestore
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +24,9 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDeLnNiL4SUk8MUv0nMhiXwqy75SlEiLp0',
+    authDomain: 'fir-fotos-18748.firebaseapp.com',
+    projectId: 'fir-fotos-18748',
+    storageBucket: 'fir-fotos-18748.appspot.com',
+    messagingSenderId: '920793332995',
+    appId: '1:920793332995:web:e224936df5e69d641fbd36',
+    measurementId: 'G-TDGPPL90CN'
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
